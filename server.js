c567d2b960f246f4f55ff11c5d9b92df51f652d1
pakaphon pankong <pakaphon23@gmail.com>
2020-10-24 20:02:52 +0700

    chore: add prettierrc

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-require("dotenv").config()
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+require('dotenv').config();
 
-mongoose.connect(process.env.DATABASE,{
-          useNewUrlParser: true,
-          useCreateIndex: true
-}).then(() => console.log("DB Connected"))
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log('DB Connected'));
 
-
-app.get("/",(req,res) => {
-          res.send("Welcome to API!!!")
-})
-
+app.get('/', (req, res) => {
+  res.send('Welcome to API!!!');
+});
 
 const port = process.env.PORT || 8080;
 
-app.listen(port,() => {
-          console.log(`Server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
