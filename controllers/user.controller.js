29e85840f48927c473dadec628009b9fc37d3e62
pakaphon pankong <pakaphon23@gmail.com>
2020-10-26 17:12:18 +0700

    feat: signup user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,9 +1,10 @@
 const Service = require('../services/user.service');
+const { errorHandler } = require('../helpers/dbErrorHandler');
 exports.signUp = async (req, res) => {
   try {
     const result = await Service.signup(req.body);
     res.status(200).json(result);
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(400).json({ error: errorHandler(error.message) });
   }
 };
--- services/user.service.js
+++ services/user.service.js
@@ -1,8 +1,8 @@
+const { errorHandler } = require('../helpers/dbErrorHandler');
 const User = require('../models/user');
 
 exports.signup = async (user) => {
   const userModel = new User(user);
-  const insertEntry = await userModel.save();
 
-  return insertEntry;
+  return userModel.save();
 };
