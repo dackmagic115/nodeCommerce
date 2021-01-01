a3acabd60b737477b1bdceec12e30b802c611919
pakaphon pankong <pakaphon23@gmail.com>
2021-01-02 00:29:01 +0700

    category CRUD

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,4 +1,19 @@
+const { errorHandler } = require('../helpers/dbErrorHandler');
+const Category = require('../models/category');
 const Service = require('../services/category.service');
+
+exports.categoryById = (req, res, next, id) => {
+  Category.findById(id).exec((err, category) => {
+    if (err || !category) {
+      return res.status(400).json({
+        error: 'Category does not exist',
+      });
+    }
+    req.category = category;
+    next();
+  });
+};
+
 exports.create = async (req, res) => {
   try {
     const result = await Service.create(req.body);
@@ -9,4 +24,46 @@ exports.create = async (req, res) => {
       err: errorHandler(error),
     });
   }
+};
+
+exports.read = (req, res) => {
+  return res.json(req.category);
+};
+
+exports.update = (req, res) => {
+  const category = req.category;
+  category.name = req.body.name;
+  category.save((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json(data);
+  });
+};
+
+exports.remove = (req, res) => {
+  const category = req.category;
+  category.remove((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json({
+      message: 'Category deleted',
+    });
+  });
+};
+
+exports.list = (req, res) => {
+  Category.find().exec((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json();
+  });
 };
--- routers/category.js
+++ routers/category.js
@@ -1,11 +1,22 @@
 const router = require('express').Router();
-const { create } = require('../controllers/category.controller');
+const {
+  create,
+  categoryById,
+  read,
+  update,
+  remove,
+  list,
+} = require('../controllers/category.controller');
 
 const { isAdmin, isAuth, requireSignin } = require('../middleware/auth');
 const userController = require('../controllers/user.controller');
-
+router.get('', list);
+router.get(':categoryId', read);
 router.post('/:userId', requireSignin, isAuth, isAdmin, create);
+router.put(':categoryId', update);
+router.delete(':categoryId', remove);
 
+router.param('categoryId', categoryById);
 router.param('userId', userController.userById);
 
 module.exports = router;
